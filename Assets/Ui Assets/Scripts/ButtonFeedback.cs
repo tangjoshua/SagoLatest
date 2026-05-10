3781961eab301747c711ce8ba266185cb475e058
BoeyChin <chinboey05@gmail.com>
2026-05-11 00:38:00 +0800

    Main Menu Scene build
    Main Menu Scene build

---
+++ Assets/Ui Assets/Scripts/ButtonFeedback.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonFeedback : MonoBehaviour,
+    IPointerDownHandler,
+    IPointerUpHandler
+{
+    public Image buttonImage;
+
+    public Sprite normalSprite;
+    public Sprite pressedSprite;
+
+    public AudioSource audioSource;
+
+    public string sceneToLoad = "GameScene";
+
+    private Vector3 originalScale;
+
+    private bool isPressed = false;
+
+    public float clickDelay = 0.3f;
+
+    void Start()
+    {
+        originalScale = transform.localScale;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (isPressed) return;
+
+        isPressed = true;
+
+        buttonImage.sprite = pressedSprite;
+
+        transform.localScale = originalScale * 1.1f;
+
+        audioSource.Play();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        StartCoroutine(ButtonRelease());
+    }
+
+    IEnumerator ButtonRelease()
+    {
+        // 恢复原图
+        buttonImage.sprite = normalSprite;
+
+        // 恢复大小
+        transform.localScale = originalScale;
+
+        // 等动画感觉结束
+        yield return new WaitForSeconds(clickDelay);
+
+        // 切换场景
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
