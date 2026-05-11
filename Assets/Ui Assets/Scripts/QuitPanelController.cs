601d10f0efdd383442c080e290d7ff1e324c71a2
BoeyChin <chinboey05@gmail.com>
2026-05-11 12:04:09 +0800

    Quit Panel added

---
+++ Assets/Ui Assets/Scripts/QuitPanelController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class QuitPanelController : MonoBehaviour
+{
+    public GameObject quitPanel;
+
+    public AudioSource buttonAudio;
+
+    public float closeDelay = 0.3f;
+
+    public void OpenQuitPanel()
+    {
+        quitPanel.SetActive(true);
+    }
+
+    public void CloseQuitPanel()
+    {
+        StartCoroutine(ClosePanelDelay());
+    }
+
+    IEnumerator ClosePanelDelay()
+    {
+        yield return new WaitForSeconds(closeDelay);
+
+        quitPanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+        Debug.Log("Game Quit");
+    }
+}
